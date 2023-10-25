a0c35d4e8bbc89f7ec2c46a87a527c075654d40a
Avas <avas.bajracharya@renegadeinsurance.com>
2023-10-25 11:45:51 +0545

    feat: serve static files

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+const publicDirName = path.join(__dirname, "../public");
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express");
-});
+app.use(express.static(publicDirName));
 
 app.listen(3000, () => {
   console.log("Server is listening at port 3000");
