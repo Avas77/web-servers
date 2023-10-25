8eccf6cbd59da59b6fc0e7dd36eff934fd801a0b
Avas <avas.bajracharya@renegadeinsurance.com>
2023-10-25 12:54:01 +0545

    feat: set up hbs for dynamic templating

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
+app.set("view engine", "hbs");
 const publicDirName = path.join(__dirname, "../public");
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "TODO app",
+    name: "Avas Bajracharya",
+  });
+});
 app.use(express.static(publicDirName));
 
 app.listen(3000, () => {
