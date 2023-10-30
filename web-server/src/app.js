d8c1a0daa03a233fa5f964951a892881ae98952e
Avas <avas.bajracharya@renegadeinsurance.com>
2023-10-30 21:25:51 +0545

    feat: added weather endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
 
@@ -45,8 +47,24 @@ app.get("/weather", (req, res) => {
       error: "You must provide address",
     });
   }
-  res.send({
-    address: req.query.address,
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({
+        error,
+      });
+    }
+    forecast(latitude, longitude, (error, weatherReport) => {
+      if (error) {
+        return res.send({
+          error,
+        });
+      }
+      res.send({
+        forecast: weatherReport,
+        location,
+        address: req.query.address,
+      });
+    });
   });
 });
 
